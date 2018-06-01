ff0d9ea53eabc6a7fa29f41e836a5a2c0b0b22c3
bsthomson <thomson.bs@gmail.com>
2018-06-01 00:03:37 -0500

    whatever

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,54 @@
     var words = ["dog", "cat", "pig", "fog", "sad"];
-
-    var word = words[Math.floor(Math.random() * words.length)];
-
+    var guessInput = document.getElementById("guess");
+    var guessOutput = document.getElementById("Wrong-Guess");
+    var word;
     var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
-        answerArray[i] = "_";
-    }
-
-    var remainingLetters = word.length;
-
-    while (remainingLetters > 0) {
-        document.getElementById("wordSpace").innerHTML = answerArray.join(" ");
-        var guess;
-        document.getElementById("wordSpace").onkeyup = function() {
-            
+    
+    function init() {
+        word = words[Math.floor(Math.random() * words.length)]
+        answerArray = [];
+        for (var i = 0; i < word.length; i++) {
+            answerArray[i] = "_";
         }
-        if (guess ===null) {
-            break;
-        } else if (guess.length !==1) {
-            alert("Please enter a single letter.");
-        } else {   
-        };
+        document.getElementById("Word-Space").innerHTML = answerArray.join(" ");
+    }
+    
+    
+
+        
+   function checkGuess() {
+       var guess = document.getElementById("guess").value;
+       var showThisMessage = "";
+            
+    }
+    if (guess.length !== 1) {
+        showThisMessage = "Please enter only a single letter";
+    } else { 
+        var j=0;
         for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
-                answerArray[j] = guess;
-                remainingLetters--;
+        if (word[j] === guess) {
+            answerArray[j] = guess;
+            showThisMessage = "Good job. " + guess + "is right.";
+        }
+        var remaining_letters = answerArray.length;
+        for (i = 0; i < answerArray.length; i++) {
+            if (answerArray[i] !== '_') {
+                remaining_letters -= 1;
             }
         }
+        if (remaining_letters == 0) {
+            showThisMessage = "Good job";
+        }
+        if (showThisMessage === "") {
+            showThisMessage = "Absolutely no "+guess;
+        }
+            document.getElementById("answer").innerHTML = answerArray.join(" ");
+            document.getElementById("guess").value = "";
+        }
+        document.getElementById("message").innerHTML = showThisMessage;
     }
 
-    document.getElementById("victoryStatus").innerHTML = answerArray.join(" ");
-        document.getElementById("victoryStatus").innerHTML = "Good job! The answer was " + word;
+
+    document.getElementById("Victory-Status").innerHTML = answerArray.join(" ");
+        document.getElementById("Victory-Status").innerHTML = "Good job! The answer was " + word;
 
